d97ff9644940d998aa36883787b10e3b59219b41
Michael Hulet <bestraysfan@me.com>
2016-03-15 01:08:48 -0500

    More semantic variable declarations

--- slack.js
+++ slack.js
@@ -1,22 +1,22 @@
 "use strict";
-let Botkit = require("botkit");
-let http = require("http");
+const Botkit = require("botkit");
+const HTTP = require("http");
 
 if(!process.env.BT_CLIENT_ID || !process.env.BT_CLIENT_SECRET || !process.env.PORT || !process.env.BT_VERIFICATION_TOKEN){
-    console.log('Error: Specify BT_CLIENT_ID, BT_CLIENT_SECRET, BT_VERIFICATION_TOKEN and BT_PORT in environment');
+    console.log('Error: Specify BT_CLIENT_ID, BT_CLIENT_SECRET, BT_VERIFICATION_TOKEN and PORT in environment');
     process.exit(1);
 }
 
 let config = {};
 if(process.env.MONGOLAB_URI){
-  let BotkitStorage = require('botkit-storage-mongo');
+  const BotkitStorage = require('botkit-storage-mongo');
   config = {storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI})};
 }
 else{
   config = {json_file_store: './db_slackbutton_slash_command/'};
 }
 
-let controller = Botkit.slackbot(config).configureSlackApp({clientId: process.env.BT_CLIENT_ID, clientSecret: process.env.BT_CLIENT_SECRET, scopes: ['commands']});
+const controller = Botkit.slackbot(config).configureSlackApp({clientId: process.env.BT_CLIENT_ID, clientSecret: process.env.BT_CLIENT_SECRET, scopes: ['commands']});
 
 controller.setupWebserver(process.env.PORT, (error, webserver) => {
   controller.createWebhookEndpoints(controller.webserver);
@@ -33,35 +33,42 @@ controller.setupWebserver(process.env.PORT, (error, webserver) => {
 controller.on('slash_command', (slashCommand, message) => {
   switch(message.command){
     case "/bibletag":
-      if (message.token !== process.env.BT_VERIFICATION_TOKEN){
+      const lc = message.text.toLowerCase()
+      if(message.token !== process.env.BT_VERIFICATION_TOKEN){
         return;
       }
       else{
-        if (message.text === "" || message.text === "help") {
+        if(lc === "" || lc === "help"){
           slashCommand.replyPrivate(message, "I'll give you a bible passage for whatever you want. Type `/bibletag [your tags]`, and I'll give you a related passage.");
           return;
         }
-        slashCommand.replyPublicDelayed(message, "", () => {
-          http.get("http://45.55.144.141:8080/tag/" + message.text.split(" ")[0], (response) => {
-            if(response.statusCode === 204){
-              slashCommand.replyPublicDelayed(message, "Sorry, but I could not find any passages for " + message.text + " :cry:");
-            }
-            else{
-              let responseString = "";
-              response.on("data", (data) => {
-                responseString += data;
-              });
-              response.on("end", () => {
-                let verses = JSON.parse(responseString);
-                let verse = verses[Math.floor(Math.random() * verses.length)];
-                slashCommand.replyPublicDelayed(message, ">" + verse.verse_text + " -" + verse.book_name + " " + verse.chapter_id + ":" + verse.verse_id);
-              });
-              response.on("error", () => {
-                slashCommand.replyPublicDelayed(message, "Something went wrong...");
-              });
-            }
+        else if(lc.split(" ")[0] === "add"){
+          //TODO: Tag a verse
+        }
+        else{
+          slashCommand.replyPublic(message, "", () => {
+            //TODO: Change to bibletag.xyz:8080 on next iteration
+            HTTP.get("http://45.55.144.141:8080/tag/" + lc.split(" ")[0], (response) => {
+              if(response.statusCode === 204){
+                slashCommand.replyPublicDelayed(message, "Sorry, but I could not find any passages for " + message.text + " :cry:");
+              }
+              else{
+                let responseString = "";
+                response.on("data", (data) => {
+                  responseString += data;
+                });
+                response.on("end", () => {
+                  const verses = JSON.parse(responseString);
+                  const verse = verses[Math.floor(Math.random() * verses.length)];
+                  slashCommand.replyPublicDelayed(message, ">" + verse.verse_text + " -" + verse.book_name + " " + verse.chapter_id + ":" + verse.verse_id);
+                });
+                response.on("error", () => {
+                  slashCommand.replyPublicDelayed(message, "Something went wrong...");
+                });
+              }
+            });
           });
-        });
+        }
       }
       break;
     default:
